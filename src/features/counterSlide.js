8306569bb5ec322fff0983bfe9545317e95ea396
Chuong Tang <chuong.tang@aol.com>
2021-05-04 19:33:14 -0700

    Add files via upload
    Add files via upload

---
+++ src/features/counterSlide.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+// populate the core of our Redux logic in Slice
+// createSlice is a function that accepts an initial state, an object full of reducer functions, and a "slice name", and automatically generates action creators and action types that correspond to the reducers and state.
+
+const initialState = { value: 0 }
+
+const Slice = createSlice({
+  name: 'counter',
+  initialState,
+    //  Reducer is acting as an "event listener" that handles events based on received action type while Dispatching actions is "triggering events"
+  reducers: {
+    increment(state) {
+      state.value++
+    },
+    decrement(state) {
+      state.value--
+    },
+    incrementByAmount(state, action) {
+      state.value += action.payload
+    },
+    decrementByAmount(state, action) {
+      state.value -= action.payload
+    },
+  },
+})
+
+export  const selectCount = (state) => state.counter.value;
+export const { increment, decrement, incrementByAmount, decrementByAmount } = Slice.actions
+export default Slice.reducer
