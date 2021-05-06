b6dd156eb0c0d94e9910393acd34648ebf82f12a
ChuongTang <chuong.tang@aol.com>
2021-05-05 20:32:50 -0600

    added first array into database

--- server/server.js
+++ server/server.js
@@ -6,9 +6,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let storeList = [
+  {id: 35245558, storename: 'FreeCovid', count: 50},
+  {id: 26134447, storename: 'MoreCovid', count: 70}
+];
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from my STORE server!" });
   });
+
+app.get('/api/stores', (req, res)=>{
+    res.send(storeList);
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
