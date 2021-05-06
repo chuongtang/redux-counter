cc5337b365941549e360994c08ed8c4d9e01c6d0
ChuongTang <chuong.tang@aol.com>
2021-05-06 13:02:38 -0600

    update API route in ApiData

--- src/features/ApiData.js
+++ src/features/ApiData.js
@@ -3,7 +3,7 @@ function ApiData() {
     const [data, setData] = useState(null);
   
     useEffect(() => {
-      fetch("/api/stores")
+      fetch("http://localhost:5000/stores")
         .then((res) => res.json())
         .then((data) => setData(data.count));
     }, []);
